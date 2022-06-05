9c74b46c88547e7d6791ef373b0930a5cba1e757
Илья <llya.tumanow2015@yandex.ru>
2022-06-05 18:15:13 +0300

    commit 2

--- src/main/java/com/gbElliasFileStorage/Controller.java
+++ src/main/java/com/gbElliasFileStorage/Controller.java
@@ -1,14 +1,20 @@
 package com.gbElliasFileStorage;
 
 import javafx.application.Platform;
+import javafx.beans.property.SimpleStringProperty;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 
+import java.io.IOException;
 import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.ResourceBundle;
+import java.util.stream.Collectors;
 
 public class Controller implements Initializable {
 
@@ -30,5 +36,28 @@ public class Controller implements Initializable {
     @Override
     public void initialize(URL location, ResourceBundle resources) {
 
+TableColumn<FileInfo,String>fileTypeColumn=new TableColumn<>();
+//для каждого элемента таблицы создаем собственный столбец
+fileTypeColumn.setCellValueFactory(param -> new SimpleStringProperty(param.getValue().getType().getName()));//как создать столбец
+fileTypeColumn.setPrefWidth(24);
+
+        TableColumn<FileInfo,String>fileNameColumn=new TableColumn<>("Имя");
+//для каждого элемента таблицы создаем собственный столбец
+        fileTypeColumn.setCellValueFactory(param -> new SimpleStringProperty(param.getValue().getFileName()));//как создать столбец
+        fileTypeColumn.setPrefWidth(240);
+
+filesTable.getColumns().addAll(fileTypeColumn,fileNameColumn);//добавить столбец в таблицу
     }
+public void updateList (Path path){//метод который умеет из какого -то пути  по любому пути собрать список файлов
+
+    try {
+        filesTable.getItems().clear();//запрос списка элементов,которые лежат в таблице
+        filesTable.getItems().addAll(Files.list(path).map(FileInfo::new).collect(Collectors.toList()));
+    } catch (IOException e) {
+        e.printStackTrace();
+    }
+
+}
+
+
 }
